f1d51afe3ef11539a50f14168a95552da2305097
RqlixMcBot <43500837+RqlixMcBot@users.noreply.github.com>
2018-11-15 16:56:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ const moment = require("moment");
 const { Client, Util } = require('discord.js');  
 const UserBlocked = new Set(); 
 const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
 const stripIndents = require('common-tags').stripIndents;
 const figlet = require('figlet');
 const google = require('google-it'); 
